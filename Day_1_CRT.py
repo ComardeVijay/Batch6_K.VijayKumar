db8a9a7e66b8cb22bac32b6af3c5d4c39f2ba48b
ComardeVijay <162981898+ComardeVijay@users.noreply.github.com>
2024-03-11 18:40:06 +0530

    Add files via upload

---
+++ Day_1_CRT.py
@@ -0,0 +1,132 @@
+#print("Hello")                        // For single line comment #  
+"""print("Hello World")                //For multple line comment """""" or '''.....'''
+print("Hello World")                   //While commmenting commentted lines of Code wil not be executed
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")"""
+#import this          #import is a keyword which is used to communicate between the python files
+
+
+"""Bargained characters
+
+Controls and Data structures → ABC language
+
+Strings and its formats from C
+
+Regular expressions Pearl
+
+Exception handling → Module 3
+
+Magic methods from fortron"""
+
+
+"""What kind of language is python
+
+Python is a
+
+A Object oriented Programing language
+
+Dynamically typed Lariguage
+
+Scripting Language
+
+A Interpreted Language
+
+Compiled Language
+
+Functional Programing Language"""
+
+
+"""Flavours of Python
+
+Cpython - Written in C, Most common Implementation of python.
+
+Jython - Java implementation of Python.
+
+IronPython - implementation of the Python programming language which is tightly integrated with .NET.
+
+Brython-Browser Python, runs in browsers
+
+RubyPython - Bridge between Python and Ruby Interpreters
+
+MicroPython - Runs in Micro Controllers"""
+
+
+
+
+'''
+#import antigravity
+class arithmatic:
+    def sum(self,a,b):
+        print(a+b)
+    def sub(self,a,b):
+        print(a-b)
+class scienific:
+    def sin(self):
+        print("its sin function")
+obj1 = arithmatic()
+operation =input("Enter the operation : ")
+if operation=="+":
+    obj1.sum(2,3)
+elif operation=="-":
+    obj1.sub(3,4)
+
+a=7
+b=8
+print(a,b)
+c=a+t
+print(c)
+
+
+print(object,sep,end,file,flush)
+print has 5 parameters
+
+object  -->the value to be printed is object
+sep --> seprator
+Eg:
+ a=8
+ b=9
+ print(a,b,sep='\n')
+
+ end--> end
+
+ print(str(a),end='')
+ print(str(b))
+
+ #!------>variables
+ variable is a name which is uesed to identify the vlaue of avriable
+ which is stored in a specific memory location
+
+ # !-----> Rules to define variables
+1) Variable have to start with only alphabets , underscores
+2) The char following the 1st  char can be alphabets , underscores,numbers
+3) No spaces are allowed
+4) No special char like(!@#$%^&*)
+5) Case sensitive
+6) Keywords are not allowed as variables
+
+
+#....>Ways to define variables
+a=8
+b=9
+print(a,b)
+
+a=7 , b=9
+print(a,b)      #Error
+
+a,b=7,9
+print(a)
+print(b)
+
+
+a=7
+b=7
+
+a=b=7
+print(a,b)
+
+a=8,7
+print(a)    #Error
+'''
+
